6da715d1947f84f9b3fa48afbc769e9f58226688
Scott <scott@lorenvale.com>
2025-08-13 11:24:43 -0700

    feat: working asteroid field

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        color = "white"
+        width = 2
+        pygame.draw.circle(screen, color, self.position, self.radius, width)
+
+    def update(self, dt):
+        self.position += self.velocity * dt 
--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 import pygame
 from constants import *
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
+    clock = pygame.time.Clock()
+    dt = 0 # seconds
+    fps = 60 # int
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        updatable.update(dt)
+    
         screen.fill("pink")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit framerate to 60 fps
+        dt = clock.tick(fps) / 1000
 
 if __name__ == "__main__":
     main()
