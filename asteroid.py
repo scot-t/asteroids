238b6eb6e7452d3826b223f36f15e1aa9a4b9333
Scott <scott@lorenvale.com>
2025-08-13 23:26:01 -0700

    fix: parameterise

--- asteroid.py
+++ asteroid.py
@@ -17,18 +17,19 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
+
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
+        
         new_radius = self.radius - ASTEROID_MIN_RADIUS
         split_angle = random.uniform(20, 50)
         asteroid_split_speed = 1.2
 
+        splits = 2
         children = []
-        for i in range(0,2):
+        for i in range(0,splits):
             children.append(Asteroid(self.position.x, self.position.y, new_radius))
         
         children[0].velocity = self.velocity.rotate(split_angle) * asteroid_split_speed
         children[1].velocity = self.velocity.rotate(-split_angle) * asteroid_split_speed
-
-
 
